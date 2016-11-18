4778ea468b4003c54379b67abe0761d77c701e16
daguanr <daguanr@gmail.com>
2016-11-18 10:35:32 +0800

    测试

--- 系统/mem.go
+++ 系统/mem.go
@@ -3,17 +3,17 @@ package main
 import (
 	"fmt"
 
-	"github.com/shirou/gopsutil/cpu"
+	"github.com/shirou/gopsutil/mem"
 )
 
 func main() {
-	//v, _ := mem.VirtualMemory()
+	v, _ := mem.VirtualMemory()
 
-	// almost every return value is a struct
-	//fmt.Printf("Total:%v, Free:%v, UsedPercent:%f%%\n", v.Total, v.Free, v.UsedPercent)
+	//almost every return value is a struct
+	fmt.Printf("Total:%v, Free:%v, UsedPercent:%f%%\n", v.Total, v.Free, v.UsedPercent)
 
-	// convert to JSON. String() is also implemented
-	//fmt.Println(v)
-	t, _ := cpu.Times(false)
-	fmt.Println(t[0].User)
+	//convert to JSON. String() is also implemented
+	fmt.Println(v)
+	//t, _ := cpu.Times(false)
+	//fmt.Println(t[0].User)
 }
